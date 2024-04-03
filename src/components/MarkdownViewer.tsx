0999681316f99b13ea2c872eb4331668e8f91c62
saseungg <yeon14766@gmail.com>
2024-04-03 18:51:57 +0900

    feat: 코드 하이라이팅 처리

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,6 +1,45 @@
+'use client';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
-  return <ReactMarkdown className='prose' remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, ref, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return match ? (
+            <SyntaxHighlighter language={match[1]} PreTag="div" {...props} style={materialDark}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props}>{children}</code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || ''}
+            alt={image.alt || ''}
+            width={500}
+            height={350}
+          />
+        ),
+        pre({ node, children, style, ...props }) {
+          return (
+            <pre {...props} style={{ ...style, backgroundColor: '#2f2f2f' }}>
+              {children}
+            </pre>
+          );
+        },
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 }
