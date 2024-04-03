b672ea2865417f3bcb8ab57ca9854c306795d983
saseungg <yeon14766@gmail.com>
2024-04-04 01:53:06 +0900

    feat: post typo style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,29 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#464646',
+            h2: {
+              fontSize: '21px',
+              fontWeight: '600',
+            },
+            h3: {
+              fontSize: '18px',
+            },
+            'blockquote p': {
+              fontWeight: '400',
+              fontStyle: 'normal',
+              lineHeight: '1.6em',
+              padding: '.5em .5em',
+              opacity: '50%',
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
