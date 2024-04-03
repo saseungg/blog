40a8edba8fc5f3d87c3c38c3bb4ee93149ea02e8
saseungg <yeon14766@gmail.com>
2024-04-03 18:10:35 +0900

    feat: post typo 적용

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,5 +2,5 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 export default function MarkdownViewer({ content }: { content: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return <ReactMarkdown className='prose' remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
 }
