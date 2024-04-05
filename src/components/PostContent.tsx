969b8dff4a257789e6b9e29da238d157fd5fadf7
saseungg <yeon14766@gmail.com>
2024-04-05 17:17:36 +0900

    feat: postcontent 스타일 수정

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -8,16 +8,16 @@ export default function PostContent({ post }: { post: PostData }) {
   return (
     <section>
       {/* 상단 영역 */}
-      <Link className="flex items-center gap-1 mb-6 w-fit hover:opacity-50" href="/">
-        <GoArrowUpLeft />
-        <p className="text-[17px] font-newsreader ">home</p>
+      <Link className="flex items-center gap-[3px] mb-6 w-fit hover:opacity-50" href="/">
+        <GoArrowUpLeft fill="var(--text-default)" />
+        <p className="text-[17px] font-newsreader text-custom-textDefault ">home</p>
       </Link>
       {/* 타이틀, 설명, 날짜 */}
       <div className="mb-12 flex flex-col gap-2 font-gowun">
         <h1 className="text-xl font-bold">{title}</h1>
-        <p className="text-[15px] text-gray-1000">{description}</p>
+        <p className="text-[15px] text-gray-1000 text-custom-textDesc2">{description}</p>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 text-custom-textDesc2">
           <a
             className="text-sm font-medium border-b border-custom-border hover:opacity-50"
             href="https://github.com/saseungg"
